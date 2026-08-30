d7eb12c838640578ec379caf1afdab80e4a4fdf2
Marcy Fletcher <fletchermarcy0@gmail.com>
2026-08-30 10:57:32 +0500

    feat: themes setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
 import "./globals.css";
 import {Geist} from "next/font/google";
 import {cn} from "@/lib/utils";
+import {ThemeProvider} from "@/components/theme-provider";
 
 const geist = Geist({subsets: ['latin'], variable: '--font-sans'});
 
 export default function RootLayout({children}: LayoutProps<"/">) {
     return (
-        <html lang="en" className={cn("font-sans", geist.variable)}>
-        <body>{children}</body>
+        <html lang="en" className={cn("font-sans", geist.variable)} suppressHydrationWarning>
+        <body>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+            {children}
+        </ThemeProvider>
+        </body>
         </html>
     );
 }
