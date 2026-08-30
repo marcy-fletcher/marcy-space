f12507dccde7dcd9550a9189338bc9bdb6b99341
Marcy Fletcher <fletchermarcy0@gmail.com>
2026-08-30 10:16:09 +0500

    feat: packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import "./globals.css";
+import {Geist} from "next/font/google";
+import {cn} from "@/lib/utils";
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+const geist = Geist({subsets: ['latin'], variable: '--font-sans'});
+
+export default function RootLayout({children}: LayoutProps<"/">) {
+    return (
+        <html lang="en" className={cn("font-sans", geist.variable)}>
+        <body>{children}</body>
+        </html>
+    );
 }
